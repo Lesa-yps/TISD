dce4d19cb2736b5d5522f696b12b54845cd2182e
Olesya <r.tevo@yandex.ru>
2023-12-06 22:39:44 +0300

    Build_res.c

--- lab_06_01/src/build_res.c
+++ lab_06_01/src/build_res.c
@@ -298,4 +298,174 @@ int build_res_add(void)
 
 
 // Сравнивается эффективность поиска в зависимости от высоты деревьев и степени их ветвления
-void build_res_find(void);
+void build_res_find(void)
+{
+    srand(time);
+    printf("Результат сравнения функций добавления в дерево и в файл:\n");
+    int rc = OK;
+    FILE *file;
+    ft_table_t *table = ft_create_table();
+
+    char buffer[SIZE_OF_BUF];
+
+    // времена числа
+    double time_start = 0.0;
+    // общее время добавления 5 элементов
+    double time_add_file_5_side = 0.0;
+    double time_add_tree_5_side = 0.0;
+    double time_add_file_5_balance = 0.0;
+    double time_add_tree_5_balance = 0.0;
+    double time_add_file_5_random = 0.0;
+    double time_add_tree_5_random = 0.0;
+    long int mem_tree_5 = 0;
+    long int mem_file_5 = 0;
+    // общее время добавления 10 элементов
+    double time_add_file_10_side = 0.0;
+    double time_add_tree_10_side = 0.0;
+    double time_add_file_10_balance = 0.0;
+    double time_add_tree_10_balance = 0.0;
+    double time_add_file_10_random = 0.0;
+    double time_add_tree_10_random = 0.0;
+    long int mem_tree_10 = 0;
+    long int mem_file_10 = 0;
+    // общее время добавления 20 элементов
+    double time_add_file_20_side = 0.0;
+    double time_add_tree_20_side = 0.0;
+    double time_add_file_20_balance = 0.0;
+    double time_add_tree_20_balance = 0.0;
+    double time_add_file_20_random = 0.0;
+    double time_add_tree_20_random = 0.0;
+    long int mem_tree_20 = 0;
+    long int mem_file_20 = 0;
+
+    // времена строками
+    // общее время добавления 5 элементов
+    char time_add_file_5_side_char[SIZE_OF_BUF];
+    char time_add_tree_5_side_char[SIZE_OF_BUF];
+    char time_add_file_5_balance_char[SIZE_OF_BUF];
+    char time_add_tree_5_balance_char[SIZE_OF_BUF];
+    char time_add_file_5_random_char[SIZE_OF_BUF];
+    char time_add_tree_5_random_char[SIZE_OF_BUF];
+    char mem_tree_5_char[SIZE_OF_BUF];
+    char mem_file_5_char[SIZE_OF_BUF];
+    // общее время добавления 10 элементов
+    char time_add_file_10_side_char[SIZE_OF_BUF];
+    char time_add_tree_10_side_char[SIZE_OF_BUF];
+    char time_add_file_10_balance_char[SIZE_OF_BUF];
+    char time_add_tree_10_balance_char[SIZE_OF_BUF];
+    char time_add_file_10_random_char[SIZE_OF_BUF];
+    char time_add_tree_10_random_char[SIZE_OF_BUF];
+    char mem_tree_10_char[SIZE_OF_BUF];
+    char mem_file_10_char[SIZE_OF_BUF];
+    // общее время добавления 20 элементов
+    char time_add_file_20_side_char[SIZE_OF_BUF];
+    char time_add_tree_20_side_char[SIZE_OF_BUF];
+    char time_add_file_20_balance_char[SIZE_OF_BUF];
+    char time_add_tree_20_balance_char[SIZE_OF_BUF];
+    char time_add_file_20_random_char[SIZE_OF_BUF];
+    char time_add_tree_20_random_char[SIZE_OF_BUF];
+    char mem_tree_20_char[SIZE_OF_BUF];
+    char mem_file_20_char[SIZE_OF_BUF];
+
+    // элементы идут по возрастанию
+    // 5 элементов
+    // добавление в файл
+    time_add_file_5_side = clock_add_file(5, f_plus);
+    sprintf(time_add_file_5_side_char, "%f", time_add_file_5_side / COUNT_SORT);
+    // добавление в дерево
+    time_add_tree_5_side = clock_add_tree(5, f_plus);
+    sprintf(time_add_tree_5_side_char, "%f", time_add_tree_5_side / COUNT_SORT);
+    // 10 элементов
+    // добавление в файл
+    time_add_file_10_side = clock_add_file(10, f_plus);
+    sprintf(time_add_file_10_side_char, "%f", time_add_file_10_side / COUNT_SORT);
+    // добавление в дерево
+    time_add_tree_10_side = clock_add_tree(10, f_plus);
+    sprintf(time_add_tree_10_side_char, "%f", time_add_tree_10_side / COUNT_SORT);
+    // 20 элементов
+    // добавление в файл
+    time_add_file_20_side = clock_add_file(20, f_plus);
+    sprintf(time_add_file_20_side_char, "%f", time_add_file_20_side / COUNT_SORT);
+    // добавление в дерево
+    time_add_tree_20_side = clock_add_tree(20, f_plus);
+    sprintf(time_add_tree_20_side_char, "%f", time_add_tree_20_side / COUNT_SORT);
+
+    // элементы сбалансированны
+    // 5 элементов
+    // добавление в файл
+    time_add_file_5_balance = clock_add_file(5, f_balance);
+    sprintf(time_add_file_5_balance_char, "%f", time_add_file_5_balance / COUNT_SORT);
+    // добавление в дерево
+    time_add_tree_5_balance = clock_add_tree(5, f_balance);
+    sprintf(time_add_tree_5_balance_char, "%f", time_add_tree_5_balance / COUNT_SORT);
+    // 10 элементов
+    // добавление в файл
+    time_add_file_10_balance = clock_add_file(10, f_balance);
+    sprintf(time_add_file_10_balance_char, "%f", time_add_file_10_balance / COUNT_SORT);
+    // добавление в дерево
+    time_add_tree_10_balance = clock_add_tree(10, f_balance);
+    sprintf(time_add_tree_10_balance_char, "%f", time_add_tree_10_balance / COUNT_SORT);
+    // 20 элементов
+    // добавление в файл
+    time_add_file_20_balance = clock_add_file(20, f_balance);
+    sprintf(time_add_file_20_balance_char, "%f", time_add_file_20_balance / COUNT_SORT);
+    // добавление в дерево
+    time_add_tree_20_balance = clock_add_tree(20, f_balance);
+    sprintf(time_add_tree_20_balance_char, "%f", time_add_tree_20_balance / COUNT_SORT);
+
+    // элементы рандомны
+    // 5 элементов
+    // добавление в файл
+    time_add_file_5_random = clock_add_file(5, f_rand);
+    sprintf(time_add_file_5_random_char, "%f", time_add_file_5_random / COUNT_SORT);
+    // добавление в дерево
+    time_add_tree_5_random = clock_add_tree(5, f_rand);
+    sprintf(time_add_tree_5_random_char, "%f", time_add_tree_5_random / COUNT_SORT);
+    // 10 элементов
+    // добавление в файл
+    time_add_file_10_random = clock_add_file(10, f_rand);
+    sprintf(time_add_file_10_random_char, "%f", time_add_file_10_random / COUNT_SORT);
+    // добавление в дерево
+    time_add_tree_10_random = clock_add_tree(10, f_rand);
+    sprintf(time_add_tree_10_random_char, "%f", time_add_tree_10_random / COUNT_SORT);
+    // 20 элементов
+    // добавление в файл
+    time_add_file_20_random = clock_add_file(20, f_rand);
+    sprintf(time_add_file_20_random_char, "%f", time_add_file_20_random / COUNT_SORT);
+    // добавление в дерево
+    time_add_tree_20_random = clock_add_tree(20, f_rand);
+    sprintf(time_add_tree_20_random_char, "%f", time_add_tree_20_random / COUNT_SORT);
+
+    mem_tree_5 = sizeof(struct Node) * (5 + 1);
+    mem_file_5 = sizeof(int) * 5 + sizeof(file);
+    mem_tree_10 = sizeof(struct Node) * (10 + 1);
+    mem_file_10 = sizeof(int) * 10 + sizeof(file);
+    mem_tree_20 = sizeof(struct Node) * (20 + 1);
+    mem_file_20 = sizeof(int) * 20 + sizeof(file);
+    sprintf(mem_tree_5_char, "%ld", mem_tree_5);
+    sprintf(mem_file_5_char, "%ld", mem_file_5);
+    sprintf(mem_tree_10_char, "%ld", mem_tree_10);
+    sprintf(mem_file_10_char, "%ld", mem_file_10);
+    sprintf(mem_tree_20_char, "%ld", mem_tree_20);
+    sprintf(mem_file_20_char, "%ld", mem_file_20);
+
+    printf("    Результаты сравнения эффективности поиска в дереве в зависимости от высоты и степени ветвления (время в тактах процессора, память в байтах):\n");
+    ft_u8write_ln(table, "Количество элементов", "5", "10", "20");
+    ft_u8write_ln(table, "Отсортированный (время)", time_add_file_5_side_char, time_add_tree_5_side_char, perc_double(time_add_file_5_side, time_add_tree_5_side, buffer));
+    ft_u8write_ln(table, "Сбалансированный (время)", time_add_file_5_balance_char, time_add_tree_5_balance_char, perc_double(time_add_file_5_balance, time_add_tree_5_balance, buffer));
+    ft_u8write_ln(table, "Рандомный (время)", time_add_file_5_random_char, time_add_tree_5_random_char, perc_double(time_add_file_5_random, time_add_tree_5_random, buffer));
+    ft_u8write_ln(table, "Память", mem_file_5_char, mem_tree_5_char, perc_double(mem_file_5, mem_tree_5, buffer));
+
+    //perc_double(stay_time_AO_static, stay_time_AO_dinam, buffer));
+    ft_set_border_style(table, FT_NICE_STYLE);
+    ft_set_cell_prop(table, FT_ANY_ROW, 0, FT_CPROP_TEXT_ALIGN, FT_ALIGNED_CENTER);
+    ft_set_cell_prop(table, FT_ANY_ROW, 1, FT_CPROP_TEXT_ALIGN, FT_ALIGNED_CENTER);
+    ft_set_cell_prop(table, FT_ANY_ROW, 2, FT_CPROP_TEXT_ALIGN, FT_ALIGNED_CENTER);
+    ft_set_cell_prop(table, FT_ANY_ROW, 3, FT_CPROP_TEXT_ALIGN, FT_ALIGNED_CENTER);
+    ft_set_cell_prop(table, FT_ANY_ROW, 4, FT_CPROP_TEXT_ALIGN, FT_ALIGNED_CENTER);
+    ft_set_cell_prop(table, 0, FT_ANY_COLUMN, FT_CPROP_ROW_TYPE, FT_ROW_HEADER);
+
+    printf("%s\n", ft_to_string(table));
+    ft_destroy_table(table);
+    return rc;
+}
